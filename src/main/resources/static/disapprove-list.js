e58b47766a6a326c4c42ce7e0e58967c67334528
Konstantinos Niktas <kniktas@palantir.com>
2014-12-02 11:33:48 -0800

    ITOOLS-14101 Render each disapproval as the row is added

--- src/main/resources/static/disapprove-list.js
+++ src/main/resources/static/disapprove-list.js
@@ -1,74 +1,121 @@
 require(['model/page-state', 'util/navbuilder', 'jquery'], function(state, nav, $) {
 
+  /************
+      BEGIN DISAPPROVAL PLUGIN
+  *************/
+
+  var prTotal = 0;
+  var intervalNumber = 0;
+  var baseUrl = undefined;
+  var repoId = undefined;
+
+  /*
+    Extract query information from the page's URL
+   */
+  function getUrlParam (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href)
+    if (results == null) {
+      return null
+    } else {
+      return results[1] || 0
+    }
+  }
+
+  /*
+    Apply the disapproval information to a pull request row
+   */
+  function doDisapproval (row) {
+    var prId = row.attr("data-pullrequestid");
+    $.getJSON (baseUrl + "/disapproval/disapprove/" + repoId + "/" + prId, function(data) {
+      if (!data.enabledForRepo) {
+        return;
+      }
+
+      disapprovalDiv = $("<div class=\"disapproval\"></div>")
+      if (data.disapproval) {
+        console.log("Pull Request " + prId + " is Disapproved by " + data.disapprovedBy)
+        disapprovalDiv.html($("<img></img>").attr("src", baseUrl + "/disapproval/static-content/disapprovalface-trim.png")
+                                            .css("max-height", "24px"))
+      } else {
+        console.log("Pull Request " + prId + " NOT Disapproved")
+      }
+
+      // Corner case of duplicate td's being inserted
+      if (row.find("td.disapproval").size() === 0) {
+        row.find("td.reviewers").before($("<td class=\"disapproval\"></td>").html(disapprovalDiv))
+      } else {
+        row.find("td.disapproval").html(disapprovalDiv)
+      }
+
+      if ($("table#pull-requests-table thead tr").find("th.disapproval").size() === 0) {
+        $("table#pull-requests-table thead tr").find("th.reviewers").before($("<th class=\"disapproval\">Disapproval</th>"))
+      }
+    });
+  }
+
+  /*
+    Scan for new rows without the disapprovals set up
+   */
+  function doNewDisapprovals () {
+    var prRows = $("table#pull-requests-table tbody tr.pull-request-row")
+    prRows.each(function (rowIndex) {
+      if ($(this).find("td.disapproval").size() === 0) {
+        doDisapproval($(this))
+      }
+    });
+    console.log("We've done " + prRows.size() + " PR disapprovals so far.")
+
+    // We've loaded the same # of PRs we know about, let's stop the interval
+    if (prRows.size() >= prTotal) {
+      clearInterval(intervalNumber)
+      intervalNumber = 0;
+    }
+  }
+
+
+
+
   // http://stackoverflow.com/questions/6285491/are-there-universal-alternatives-to-window-onload-without-using-frameworks
   // Decided to do with jquery, to avoid messing up the window.onload
   $(document).ready(function() {
 
-    compiledData = {}
-    disapprovalEnabled = false
     baseUrl = nav.pluginServlets().build()
     repoId = state.getRepository().id;
-    outstandingCount = 0
-    callbacks = []
 
-    tables = $("table tbody")
-    tables.each(function() {
-      $(this).find('tr').each(function() {
-        var prId = $(this).attr("data-pullrequestid")
-        outstandingCount += 1
-        prd = $.get(baseUrl + '/disapproval/disapprove/' + repoId + '/' + prId)
-        callbacks.push(prd)
+    // Make sure we're querying for the right kind of PRs
+    var prState = getUrlParam("state")
+    if (prState == null) {
+      prState = "open"
+    }
+    prRestBaseUrl = nav.rest().currentRepo().allPullRequests().build() + "?limit=100&state=" + prState
 
-        reviewers = $(this).children('.reviewers')
-          prd.success(function(data) {
+    // The rest api for pull requests won't let me get everything at once
+    var allPRsCounted = false
+    while (!allPRsCounted) {
+      $.ajax({ url: prRestBaseUrl + "&start=" + prTotal,
+               dataType:'json',
+               async: false,
+               success: function (data) {
+                 prTotal += data.size;
+                 if (data.isLastPage) {
+                   allPRsCounted = true
+                 }
+               }
+      });
+    }
+    console.log("There are " + prTotal + " total PRs")
 
-          // If things are not enabled, immediately bail out
-          disapprovalEnabled = data.enabledForRepo
-          if (!disapprovalEnabled) {
-            return;
-          }
+    // Apply to the already loaded rows
+    $("tr.pull-request-row").each(function (rowIndex) {
+      doDisapproval($(this));
+    });
 
-          compiledData[prId.toString()] = data
-        })
-        // know when we are done by tracking outstanding calls
-        prd.always(function() {
-          outstandingCount -= 1
-        })
-      })
-    })
+    if (prTotal > 25) {
+      intervalNumber = setInterval(doNewDisapprovals, 750);
+    }
 
-    // when all callbacks are done...
-    $.when.apply($, callbacks).then(function() {
-      tables.each(function() {
-        $(this).find('tr').each(function() {
-          var prId = $(this).attr("data-pullrequestid")
-          reviewers = $(this).children('.reviewers')
-
-          disapprovalHtml = "";
-          data = compiledData[prId.toString()]
-          disapprovalDiv = $("<div class=\"disapproval\" style=\"color: #AA0000\"></div>")
-          if (data.disapproval) {
-            console.log("Pull Request is Disapproved by " + data.disapprovedBy)
-            disapprovalDiv.html($("<img></img>").attr("src",baseUrl + "/disapproval/static-content/disapprovalface-trim.png")
-                                                .css("max-height", "24px"))
-          } else {
-            console.log("Pull Request NOT Disapproved")
-          }
-          reviewers.before($("<td class=\"disapproval\"></td>").html(disapprovalDiv))
-        })
-      })
-
-      // Add the header if it needs to be added
-      if (disapprovalEnabled) {
-        tableHead = $("table thead")
-        tableHead.each(function() {
-          $(this).find('tr').each(function() {
-            reviewers = $(this).children('.reviewers')
-            reviewers.before('<th class="disapproval">Disapproval</th>')
-          })
-        })
-      }
-    })
   })
+
+
 })
 
